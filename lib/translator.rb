3a759d5ed8a2771c3795c757dc21afa43c558594
Darrel C <dcastellano2007@gmail.com>
2019-08-25 19:45:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(path)
 
   emotes.each do |key, value|
     new_hash['get_meaning'][value[1]] = key
-    new_hash['get_emoticon'][key] = value[0]
+    new_hash['get_emoticon'][value[0]] = emotes[key][1]
   end
   new_hash
 end
